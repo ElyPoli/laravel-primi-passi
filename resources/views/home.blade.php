46c7cbf1779e5f66ae4433e387552b6209170088
ElyPoli <itsos.poli@gmail.com>
2023-09-22 19:36:05 +0200

    added home data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Esercizio 53: Laravel - primi passi</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap css -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    
-        <!-- Icone FontAwesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    
-        <!-- Icona -->
-        <link rel="icon" href="{{ asset('img/icon-logo.png') }}">
+    <title>Esercizio 53: Laravel - primi passi</title>
 
-        <!-- Custom css -->
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-    </head>
-    <body>
-        <h1>Hello World</h1>
-    </body>
+    <!-- Icone FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <!-- Icona -->
+    <link rel="icon" href="{{ asset('img/icon-logo.png') }}">
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+</head>
+
+<body>
+    <header class="bg-primary">
+        {{-- Menù con i generi --}}
+        <nav class="container pt-3 pb-3 text-secondary text-center">
+            <div class="row row-cols-2 row-cols-sm-4 gy-3 justify-content-center">
+                @foreach ($movieGenres as $singleGenre)
+                    <div class="col">
+                        <a href="{{ $singleGenre['route'] }}">
+                            {{ $singleGenre['genre'] }}
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </nav>
+    </header>
+
+    <main class="container pt-5 pb-5">
+        <h1 class="text-center text-primary m-0 pb-5 fw-bold">Generi cinematografici</h1>
+        <div class="row row-cols-1 row-cols-sm-2 gy-3 justify-content-center">
+            @foreach ($movieGenres as $singleGenre)
+                <div class="col">
+                    <div class="card bg-primary">
+                        <a href="{{ $singleGenre['route'] }}" class="text-secondary decoration-none">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">{{ $singleGenre['genre'] }}</h5>
+                                <p class="card-text">{{ $singleGenre['description'] }}</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </main>
+
+    <footer class="bg-primary">
+        <p class="m-0 pt-2 pb-2 text-center text-secondary">
+            <i class="fa-solid fa-chevron-right"></i>
+            Generi cinematografici
+            <i class="fa-solid fa-chevron-left"></i>
+        </p>
+    </footer>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    // Array con i dati da passare
+    $data = [
+        "movieGenres" => [
+            [
+                "genre" => "Horror",
+                "description" => "Il genere horror è noto per il suo intento di spaventare e mettere in tensione il pubblico attraverso l'uso di elementi come mostri, creature sovrannaturali, situazioni terrificanti e suspense. Questi film cercano di suscitare emozioni come la paura, l'ansia e l'orrore nei loro spettatori. L'horror può variare da storie paranormali a film basati su eventi reali o psicologici, offrendo una vasta gamma di esperienze spaventose.",
+                "route" => 1
+            ],
+            [
+                "genre" => "Sci-Fi",
+                "description" => "Il genere della fantascienza (sci-fi) esplora temi legati alla scienza, alla tecnologia e al futuro. Questi film spesso presentano avanzate tecnologie, esplorazioni spaziali, viaggi nel tempo, mondi futuristici e situazioni ipotetiche che sfidano la realtà. La fantascienza può essere utilizzata per esaminare questioni sociali, etiche e filosofiche e per immaginare un mondo diverso da quello che conosciamo oggi, offrendo una prospettiva affascinante del possibile futuro dell'umanità.",
+                "route" => 2
+            ],
+            [
+                "genre" => "Commedia",
+                "description" => "Il genere della commedia ha l'obiettivo principale di far ridere il pubblico. Questi film presentano situazioni divertenti, personaggi esilaranti e dialoghi spiritosi. La commedia può assumere molte forme, tra cui commedie romantiche, commedie nere, commedie slapstick e molto altro. L'obiettivo è far sorridere e ridere gli spettatori, spesso offrendo una pausa dall'ansia e dal dramma della vita quotidiana.",
+                "route" => 3
+            ],
+            [
+                "genre" => "Drammatico",
+                "description" => "Il genere drammatico è noto per affrontare temi seri ed emotivi attraverso storie coinvolgenti e complesse. Questi film esplorano le emozioni umane, le relazioni, le sfide personali e le lotte interiori dei personaggi. Possono trattare argomenti come l'amore, la perdita, la lotta per la sopravvivenza, le ambizioni e molto altro. Il dramma mira a suscitare empatia e riflessione negli spettatori, spesso fornendo una rappresentazione autentica e profonda delle sfide della vita.",
+                "route" => 4
+            ]
+        ]
+    ];
+
+    return view('home', $data);
 });
