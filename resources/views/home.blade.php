b18843475a5adb5edf7c6bba4d6f90ee6402ca2a
ElyPoli <itsos.poli@gmail.com>
2023-09-22 17:57:59 +0200

    added home - hello world

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Esercizio 53: Laravel - primi passi</title>
+
+        <!-- Bootstrap css -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
+        <!-- Icone FontAwesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    
+        <!-- Icona -->
+        <link rel="icon" href="{{ asset('img/icon-logo.png') }}">
+
+        <!-- Custom css -->
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
